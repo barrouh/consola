3bec5c50d28dc611c2b94e655dff0621d3cff457
Mohamed Barrouh <mohamed.barrouh@gmail.com>
2021-03-13 15:31:22 +0100

    init

---
+++ consola-backend/src/main/java/com/consola/main/Consola.java
@@ -0,0 +1,13 @@
+package com.consola.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consola {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Consola.class, args);
+	}
+
+}
