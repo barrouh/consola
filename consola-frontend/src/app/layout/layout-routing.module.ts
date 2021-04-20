044a78ebaed3a65bed9d022884c4d41c3250ca4c
Oussama El abed <elabedouss@gmail.com>
2021-04-20 01:10:31 +0000

    Design the Login form, add layout component, configure the routing.

---
+++ consola-frontend/src/app/layout/layout-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LayoutComponent } from './layout.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'landing-page', component: LandingPageComponent },
+      { path: '**', redirectTo: 'landing-page', pathMatch: 'full' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LayoutRoutingModule {}
--- consola-frontend/src/app/layout/layout.module.ts
+++ consola-frontend/src/app/layout/layout.module.ts
@@ -1,29 +1,23 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './layout.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
-import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { LayoutRoutingModule } from './layout-routing.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [LayoutComponent, LandingPageComponent, LoginComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
+    LayoutRoutingModule,
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
@@ -36,6 +30,6 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [LayoutComponent],
 })
-export class AppModule {}
+export class LayoutModule {}
