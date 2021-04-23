a4622112a8626f55f5afa8e1e7f9d307ddc4315b
Mohamed Barrouh <mohamed.barrouh@gmail.com>
2021-04-23 18:40:47 +0000

    repo conf

--- consola-backend/src/main/java/com/consola/rest/ProjectRestController.java
+++ consola-backend/src/main/java/com/consola/rest/ProjectRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.consola.model.Project;
-import com.consola.repository.ProjectRepository;
+import com.consola.repositories.ProjectRepository;
 
 @RestController
 @RequestMapping("/rest-api/project/")
