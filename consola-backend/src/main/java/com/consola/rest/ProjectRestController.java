7d8e79c053e3e4f404bcdbc74dae5d1e8d42d1f4
Oussama El abed <elabedouss@gmail.com>
2021-05-02 17:46:33 +0000

    change to supported method(POST) of save project

--- consola-backend/src/main/java/com/consola/rest/ProjectRestController.java
+++ consola-backend/src/main/java/com/consola/rest/ProjectRestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +36,7 @@ public class ProjectRestController {
 		return projectRepository.findById(id);
 	}
 	
-	@GetMapping("/save")
+	@PostMapping("/save")
 	public Project saveProject(@RequestBody ProjectDTO project) {
 		return projectRepository.saveAndFlush(mapper.map(project, Project.class));
 	}
