1622bf2bed8a64c848fccb1668e78d58ef16ce82
Oussama El abed <elabedouss@gmail.com>
2021-05-14 18:49:26 +0000

    Add employee, notification and vacation components

--- consola-frontend/src/app/layout/layout-routing.module.ts
+++ consola-frontend/src/app/layout/layout-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
-import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LayoutComponent } from './layout.component';
-import { NotificationComponent } from './notification/notification.component';
+import { NotificationListComponent } from './notification/notification-list/notification-list.component';
 import { ProjectsListComponent } from './project/projects-list/projects-list.component';
 import { RoleListComponent } from './role/role-list/role-list.component';
 import { StatusListComponent } from './status/status-list/status-list.component';
 import { SupervisorComponent } from './supervisor/supervisor.component';
 import { VacationStatusListComponent } from './vacation-status/vacation-status-list/vacation-status-list.component';
+import { VacationListComponent } from './vacation/vacation-list/vacation-list.component';
 
 const routes: Routes = [
   {
@@ -21,8 +22,9 @@ const routes: Routes = [
       { path: 'status', component: StatusListComponent },
       { path: 'role', component: RoleListComponent },
       { path: 'vacation-status', component: VacationStatusListComponent },
-      { path: 'employee', component: EmployeeComponent },
-      { path: 'notification', component: NotificationComponent },
+      { path: 'vacation', component: VacationListComponent },
+      { path: 'employee', component: EmployeeListComponent },
+      { path: 'notification', component: NotificationListComponent },
       { path: 'supervisor', component: SupervisorComponent },
       { path: 'contact', component: ContactComponent },
       { path: '**', redirectTo: 'landing-page', pathMatch: 'full' },
