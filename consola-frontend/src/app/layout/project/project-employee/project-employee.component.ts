65bef6023cff4088a9667e4826b7ea2153469add
Oussama El abed <elabedouss@gmail.com>
2021-10-03 18:04:53 +0100

    add project-employee component

---
+++ consola-frontend/src/app/layout/project/project-employee/project-employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-employee',
+  templateUrl: './project-employee.component.html',
+  styleUrls: ['./project-employee.component.css']
+})
+export class ProjectEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
